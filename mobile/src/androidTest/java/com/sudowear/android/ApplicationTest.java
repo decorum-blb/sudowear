acdac0487303090bf4a9c86ee698e7649c7ee34d
Enrico Mills <enrico@nomadlabs.com>
2014-08-23 13:30:11 -0400

    Rename package

--- mobile/src/androidTest/java/com/sudowear/android/ApplicationTest.java
+++ mobile/src/androidTest/java/com/sudowear/android/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.adam.myapplication;
+package com.sudowear.android;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- wear/src/main/java/com/sudowear/android/MyActivity.java
+++ wear/src/main/java/com/sudowear/android/MyActivity.java
@@ -1,4 +1,4 @@
-package com.example.adam.myapplication;
+package com.sudowear.android;
 
 import android.app.Activity;
 import android.os.Bundle;
