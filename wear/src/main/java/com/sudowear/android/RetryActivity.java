5af513da0c64c4739b3dc3e0168570c9594fada4
Pat Needham <needham.patrick@gmail.com>
2014-08-24 13:44:28 -0400

    Added Retry Activity and layout file, as well as custom blue oval background for image icon.

---
+++ wear/src/main/java/com/sudowear/android/RetryActivity.java
@@ -0,0 +1,25 @@
+package com.sudowear.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class RetryActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retry);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+
+}
