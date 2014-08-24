217fc7f4395bc54644d719c30ff6de1c02502fd5
Enrico Mills <enrico@nomadlabs.com>
2014-08-24 11:51:41 -0400

    Message IntentService and Confirmation Activity
    Create an intent service to send messages to the phone
    Display a confirmation activity when the wearable app recognizes a command

--- wear/src/main/java/com/sudowear/android/MyActivity.java
+++ wear/src/main/java/com/sudowear/android/MyActivity.java
@@ -4,17 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.support.wearable.activity.ConfirmationActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
-import com.google.android.gms.wearable.Wearable;
-
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
 public class MyActivity extends Activity {
@@ -22,10 +16,9 @@ public class MyActivity extends Activity {
     private static final String TAG = MyActivity.class.getSimpleName();
 
     private final static int SPEECH_REQUEST_CODE = 1;
+    private final static int CONFIRMATION_REQUEST_CODE = 2;
 
     private TextView mTextView;
-
-    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +31,7 @@ public class MyActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .build();
-        mGoogleApiClient.connect();
         displaySpeechRecognizer();
-        //getNodes();
     }
 
     // Create an intent that can start the Speech Recognizer activity
@@ -62,27 +50,34 @@ public class MyActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
             Intent data) {
-        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
-            if (spokenText.toLowerCase().equals("make me a sandwich")) {
-                mTextView.setText("bitch");
-            } else {
-                mTextView.setText("y u no make sense");
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SPEECH_REQUEST_CODE) {
+                List<String> results = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                String spokenText = results.get(0);
+                if (spokenText.toLowerCase().equals("make me a sandwich") ||
+                        spokenText.toLowerCase().equals("a")) {
+
+                    MyIntentService.startSendMessage(this, "", "");
+
+                    Log.v(TAG, "Starting ConfirmationActivity");
+                    Intent intent = new Intent(this, ConfirmationActivity.class);
+                    intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE,
+                            ConfirmationActivity.SUCCESS_ANIMATION);
+                    intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, getString(R.string.dialog_success));
+                    startActivityForResult(intent, CONFIRMATION_REQUEST_CODE);
+                } else {
+                    mTextView.setText("y u no make sense");
+                }
             }
+        } else if (resultCode == RESULT_CANCELED) {
+            // Confirmation activity does not return OK
+            if (requestCode == CONFIRMATION_REQUEST_CODE) {
+                finish();
+            }
+        } else {
+            Log.v(TAG, "When does this happen?");
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
-
-    private Collection<String> getNodes() {
-        HashSet<String> results = new HashSet<String>();
-        NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
-        for (Node n : nodes.getNodes()) {
-            Log.v(TAG, n.getDisplayName());
-            results.add(n.getId());
-        }
-        return results;
-    }
-
 }
